f7a7c9226b995d785317d9ca37a72ae94333aa52
Ashwin S M <ashwinmalaiyandi@gmail.com>
2021-04-03 15:22:46 +0530

    Modified Changes

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-import Express from 'express';
+import Express, { response } from 'express';
 
 const superagent = require('superagent');
 
 const app =  new Express();
 const port = 3000;
+const client_id = "c0c3936b4710adcf7f2b";
+const client_secret = "3440cc74f000acb5a7112963286f3745304b9469";
+const CALLBACK_URL = 'https://github.com/login/oauth/access_token';
+const scope = "repo user";
+const codeVSAccessTokenMap = {};
 
 app.get('/', (request, response)=>{
+    response.sendFile('Client/signInPage.html' , { root : __dirname});
+})
+
+app.get('/home', (request, response)=>{
+    console.log("Inside Homee");
     response.sendFile('Client/homePage.html' , { root : __dirname});
 })
 
-app.get('/homePage.js', (request, response)=>{
-    response.sendFile('Client/homePage.js' , { root : __dirname});
+app.get('/getToken', (request, response)=>{
+    const {code} = request.query;
+    const access_token = codeVSAccessTokenMap[code];
+    delete codeVSAccessTokenMap[code];
+    console.log(access_token);
+    console.log(codeVSAccessTokenMap);
+    response.send(access_token)
 })
 
 app.get('/user/callback', (request, response, next)=>{
@@ -20,20 +35,31 @@ app.get('/user/callback', (request, response, next)=>{
     if(!code) {
         return response.send("Invalid Code");
     }
-
+    
     superagent
-        .post('https://github.com/login/oauth/access_token')
-        .send({ 
-            client_id: 'c0c3936b4710adcf7f2b',
-            client_secret: '3440cc74f000acb5a7112963286f3745304b9469',
+        .post(CALLBACK_URL)
+        .send({
+            client_id: client_id,
+            client_secret: client_secret,
             code : code
             })
         .set('Accept', 'application/json')
         .then(function(result){
             const data = result.body;
+            codeVSAccessTokenMap[code] = data.access_token;
             console.log(data);
+            console.log(codeVSAccessTokenMap);
+            response.send("Redirecting to GitPush chrome extension");
+        });
+})
+
+app.get('/getDirectories',(request, response, next) => {
+    superagent
+        .get('https://api.github.com/repos/AshwinSM/LeetCode/issues')
+        .set('Accept', 'application/json')
+        .then(function(data){
             response.send(data);
-        }); 
+        });
 })
 
 app.get('/user', (request, response, next) => {
@@ -46,5 +72,34 @@ app.get('/user', (request, response, next) => {
     });
 });
 
-app.listen(port)
 
+//Get CSS and JS
+app.get('/homePage.js', (request, response)=>{
+    response.sendFile('Client/homePage.js' , { root : __dirname});
+})
+
+app.get('/homePage.css', (request, response)=>{
+    response.sendFile('Client/homePage.css' , { root : __dirname});
+})
+
+app.get('/signInPage.js', (request, response)=>{
+    response.sendFile('Client/signInPage.js' , { root : __dirname});
+})
+
+app.get('/signInPage.css', (request, response)=>{
+    response.sendFile('Client/signInPage.css' , { root : __dirname});
+})
+
+app.get('/axios.min.js', (request, response)=>{
+    response.sendFile('axios.min.js' , { root : __dirname});
+})
+
+app.get('*/octocat.png', (request, response)=>{
+    response.sendFile('Images/logo.png' , { root : __dirname});
+})
+
+app.get('*/githubIcon.png', (request, response)=>{
+    response.sendFile('Images/githubIcon.png' , { root : __dirname});
+})
+
+app.listen(port);
